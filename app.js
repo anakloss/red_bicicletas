2ab26be6073db86be64b039866e78935e3f4d00f
Ana Kloss <anak492@gmail.com>
2022-06-27 12:08:01 -0300

    modif error auth google 3

--- app.js
+++ app.js
@@ -149,10 +149,11 @@ app.get('/auth/google',
   ]})
 );
 app.get('/auth/google/callback',
-  passport.authenticate('google', {
-    successRedirect: '/',
-    failureRedirect: '/error'
-  })
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  }
 );
 
 // catch 404 and forward to error handler
